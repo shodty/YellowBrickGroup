8810624b7d665ff25247e12db86d6cd30f9cc143
shodty <rob.landsburg@gmail.com>
2020-01-15 13:16:21 -0800

    form testing html static

--- src/components/FormTest.vue
+++ src/components/FormTest.vue
@@ -2,7 +2,7 @@
 #formbackground
     b-row(align-h="center")
         b-col(cols=5 md=5)  
-          form(name='mail-service' method='post' id="mailserviceform" data-netlify="true" data-netlify-honeypot="bot-field")  
+          form(name='mail-service' method='POST' id="mailserviceform" data-netlify="true" data-netlify-honeypot="bot-field")  
             input(type='hidden' name='form-name' value='ask-question')
             b-form-row( id="input-group-1"  label-for="input-1" )
                 b-col(cols="3" class="blackborder" )
